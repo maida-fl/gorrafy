55468751f514904a5ec8c95c22ae698d2281cf3c
maldonadomatias <molporron@gmail.com>
2021-08-27 15:25:13 -0300

    Correr App.js en Puerto 3000

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+
+app.listen(3000, () => {
+    console.log("Serving on port 3000!");
+});

09e8f988a78725ea3beb37250e7d1a3037231075
EmirFederico <emirfederico@gmail.com>
2021-10-16 01:17:57 -0300

    Crear Producto / Revisar Multer

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const adminController = {
     admin: (req, res) => {
         res.render('admin');
@@ -7,7 +13,25 @@ const adminController = {
     },
     editar: (req, res) => {
         res.render('adminEditar');
+    },
+    // POST para agregar producto y almacenarlo
+    store: (req, res) => {
+        const newProduct = {
+			id: products[products.length - 1].id +1,
+			name: req.body.productName,
+		    price: req.body.price,
+			category: req.body.productCategory,
+			description: req.body.productDescription,
+            colour: req.body.productColour,
+			image: req.file.filename 
+		}
+		products.push(newProduct);
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+		res.redirect("/producto");
+
     }
+
 };
 
 module.exports = adminController;
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,26 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
 // ************ Controller Require ************
 const adminController = require('../controllers/adminController');
+
+// ************ Multer ************
+const storage = multer.diskStorage({ 
+    destination: function (req, file, cb) {
+       cb(null, './public/img');
+    },
+    filename: function (req, file, cb) {
+       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+    }
+    
+})
+
+const upload = multer({storage: storage});
+
+
 
 // ************ Routes ************
 
@@ -11,6 +28,7 @@ const adminController = require('../controllers/adminController');
 router.get('/', adminController.admin)
 router.get('/editar', adminController.editar)
 router.get('/agregar', adminController.agregar)
+router.post('/', upload.single("productImage"), adminController.store)
 
 
 

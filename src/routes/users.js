2e9c5168d5db912237411965ec7d94807290ee5c
EmirFederico <emirfederico@gmail.com>
2021-11-02 21:08:09 -0300

    guestMiddleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const uploadFile = require('../middlewares/multerMiddleware');
 const validations = require('../middlewares/validateRegister');
+const guestMiddleware = require('../middlewares/guestMiddleware');
+
+
 
 // ************ Controller Require ************
 const userController = require('../controllers/userController');
@@ -10,12 +13,12 @@ const userController = require('../controllers/userController');
 // ************ Routes ************
 
 // Formulario de Register
-router.get('/register', userController.register)
+router.get('/register',guestMiddleware, userController.register)
 // Procesar el register
 router.post('/register', uploadFile.single('avatar'), validations, userController.processRegister)
 
 // Formulario de Login
-router.get('/login', userController.login)
+router.get('/login', guestMiddleware, userController.login)
 // Procesar el Login
 router.post('/login', userController.processLogin)
 

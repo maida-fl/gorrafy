cdf6c316390d8ae18e60f8c65c2cdf8767bd82f0
EmirFederico <emirfederico@gmail.com>
2021-11-13 19:17:39 -0300

    tabla de modelo User

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,37 +1,37 @@
 module.exports = (sequelize, dataTypes) => {
-    const Product = sequelize.define('Product', {
+    const User = sequelize.define('User', {
         id: {
             type: dataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true
         },
-        name: {
+        firstName: {
             type: dataTypes.STRING
         },
-        price: {
+        lastName: {
+            type: dataTypes.STRING
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        password: {
+            type: dataTypes.STRING
+        },
+        category: {
             type: dataTypes.INTEGER
         },
-        description: {
-            type: dataTypes.STRING
-        },
-        image: {
+        avatar: {
             type: dataTypes.STRING.BINARY
-        },
- /*       id_category: {
-            type: dataTypes.INTEGER
-        },
-        id_colour: {
-            type: dataTypes.INTEGER
-        },*/   
+        },  
     }, {
-        tableName: 'products',
-        timestamps: true, //Se agrego esta linea 13-nov
+        tableName: 'users',
+        timestamps: true,
         paranoid: true,
         deletedAt: "softDelete", 
         createdAt: "created_at", //estan bien? en el diagrama no incluimos marcas temporales
         updatedAt: "updated_at"  //estan bien? en el diagrama no incluimos marcas temporales
     })
     
-    return Product;
+    return User;
 }
 
